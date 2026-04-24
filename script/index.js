dae18726a0bfc3b9b19efefada967655fd768a9e
nafiio <aznafix@gmail.com>
2026-04-24 11:34:20 +0600

    added the searchBox

--- script/index.js
+++ script/index.js
@@ -149,3 +149,20 @@ const displayLesson = (lessons) => {
   });
 };
 loadLesson();
+
+document.getElementById("btn-search").addEventListener("click", () => {
+  removeActive();
+  const input = document.getElementById("input-search");
+  const searchValue = input.value.trim().toLowerCase();
+  console.log(searchValue);
+  fetch("https://openapi.programming-hero.com/api/words/all")
+    .then((res) => res.json())
+    .then((data) => {
+      const allWords = data.data;
+      console.log(allWords);
+      const filterAllWords = allWords.filter((word) =>
+        word.word.toLowerCase().includes(searchValue),
+      );
+      displayLevelWord(filterAllWords);
+    });
+});
